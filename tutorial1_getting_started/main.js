f9b56fd385804e45956a3f2cefb4f9706c8ff011
Kai Prenger <kai.prenger@gmail.com>
2021-02-08 07:06:15 -0500

    add better prompt message

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -8,8 +8,8 @@ var my_number = 5
 
 console.log(square(my_number) + " is " + my_number + " squared!")
 
-num_to_square = prompt("Pick a number?")
+num_to_square = prompt("Enter a number, and check for the result in the browser console!")
 
 console.log("The square of " + num_to_square + " is " + square(num_to_square))
 
-console.log("the last two lines printed to the console use a function!")
+console.log("the last two lines printed to the console use a function called square!")
