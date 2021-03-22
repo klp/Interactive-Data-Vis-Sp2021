5a87ae183bd89a1576a6a56d29b7dcb32f6091dd
Kai Prenger <kai.prenger@gmail.com>
2021-03-22 09:50:32 -0400

    draw initial treemap...

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -13,6 +13,8 @@ let tooltip;
  * */
 let state = {
   // + INITIALIZE STATE
+  data: null,
+  hover: null
 };
 
 /**
@@ -28,7 +30,9 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+  const colorScale = d3.scaleOrdinal(d3.schemeTableau10);
   const container = d3.select("#d3-container").style("position", "relative");
+  
 
   svg = container
     .append("svg")
@@ -38,12 +42,41 @@ function init() {
   // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
 
   // + CREATE YOUR ROOT HIERARCHY NODE
+  const root = d3.hierarchy(state.data)
+    .sum(d => d.value)
+    .sort((x, y) => y.value - x.value);
 
   // + CREATE YOUR LAYOUT GENERATOR
-
-  // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
-
+  const treeLayout = d3.treemap()
+    .size(
+      [ width - margin.left - margin.right,
+        height - margin.top - margin.bottom
+      ]
+    )
+    .round(true)
+  
+    // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
+  const tree = treeLayout(root)
+  const leaves = tree.leaves()
+ 
   // + CREATE YOUR GRAPHICAL ELEMENTS
+  const groups = svg 
+      .selectAll(".leaf")
+      .data(leaves)
+      .join("g")
+      .attr("class", "leaf")
+      .attr("transform", d => `translate(${d.x0}, ${d.y0})`)
+
+  // append shapes
+  groups.append("rect")
+      .attr("height", d => d.y1 - d.y0)
+      .attr("width", d => d.x1 - d.x0)
+      .attr("fill", d => {
+        const previousAncestor = d.ancestors().find(d => d.depth === 1);
+        return colorScale(previousAncestor.data.name);
+        }
+      )
+
 
   draw(); // calls the draw function
 }
